3e014e10372142f5e8561f0b49d8a3507ff47ae3
Alex Underwood <underwoodapu@gmail.com>
2020-05-07 22:07:38 -0700

    Todo

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -8,17 +8,20 @@ function init() {
     $("#total").text(total);
     $("#done").text(done);
     $("#celebrate").hide();
+    $("#clear").hide();
 }
 
 function doNotCelebrate() {
     if(done < total) {
         $("#celebrate").hide();
+        $("#clear").hide();
     }
 }
 
 function doCelebrate() {
-    if(done === total) {
+    if(done === total && total !== 0) {
         $("#celebrate").show();
+        $("#clear").show();
     }
 }
 
@@ -48,6 +51,7 @@ $("ul").on("click", "span", function(event){
             $("#done").text(done);
         }
         doCelebrate();
+        doNotCelebrate();
     });
     event.stopPropagation();
 });
@@ -66,11 +70,6 @@ $("#add").keypress(function(event){
     }
 });
 
-// add another ToDo List
-$(".fa-plus-circle").click(function(){
-    alert("add new list");
-});
-
 // expand text box
 $(".fa-edit").click(function(){
     $("#add").fadeToggle();
@@ -79,6 +78,24 @@ $(".fa-edit").click(function(){
 // hover add todo icon
 $(".fa-edit").hover(function() {
     $(this).css('cursor','pointer').attr('title', 'Add A ToDo');
+});
+
+// Clear all list items
+$("#clear").click(function(){
+    $("li").fadeOut(500, function(){
+        $("#add").fadeIn(1000);
+        // the worst line of code but it's the only thing that seems to work? .vel() does nothing
+        done = done - done;
+        total = total - total;
+        $("#total").text(total);
+        $("#done").text(done);
+        $("#celebrate").hide();
+        $("#clear").hide();
+    });
+    
+});
+$("#clear").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'Clear your list');
 });
 
 // hover remove todo icon
@@ -94,28 +111,101 @@ $("li").hover(function() {
 });
 
 // DRY this up a bit once you solve the scaling background issue
-
-$("#imageOne").click(function(){
-    $("body").css("background", "url('assets/imgs/backgrounds/rainbow-galaxy.jpeg')");
+function styleOptionalBackground(){
+    $("body").css("object-fit", "cover");
+    $("body").css("height", "100%");
+    $("body").css("background-repeat", "no-repeat");
     $("body").css("background-size", "cover");
+    $("body, html").css("min-height", "100%");
     $("body").css("-webkit-background-size", "cover");
     $("body").css("-moz-background-size", "cover");
     $("body").css("-o-background-size", "cover");
+}
+
+// BAckground reset icon listener
+$("#bgReset").click(function(){
+    $("body").css("background", "-webkit-linear-gradient(to right, #F8FFAE, #43C6AC)");
+    $("body").css("background", "linear-gradient(to right, #F8FFAE, #43C6AC)");
+    $("body").css("background", "#43C6AC");
+});
+$("#bgReset").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'Reset To Default');
+});
+
+// Font reset icon listener
+$("#fontReset").click(function(){
+    $("ul").css("font-family", "Roboto", "sans-serif");
+    $("#add").css("font-family", "Roboto", "sans-serif");
+    $(".title").css("font-family", "Roboto", "sans-serif");
+    $(".title").css("width", "80%");
+    $(".progress").css("font-family", "Roboto", "sans-serif");
+    $("#clear").css("font-family", "Roboto", "sans-serif");
+});
+$("#fontReset").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'Reset To Default');
+});
+
+// Background image listeners
+$("#imageOne").click(function(){
+    $("body").css("background", "url('assets/imgs/backgrounds/rainbow-galaxy.jpeg')");
+    styleOptionalBackground();
 });
 
 $("#imageTwo").click(function(){
     $("body").css("background", "url('assets/imgs/backgrounds/unicorn-sky.jpeg')");
-    $("body").css("background-size", "cover");
+    styleOptionalBackground();
 });
 
 $("#imageThree").click(function(){
     $("body").css("background", "url('assets/imgs/backgrounds/usa-space.jpg')");
-    $("body").css("background-size", "cover");
+    styleOptionalBackground();
 });
 
 $("#imageFour").click(function(){
     $("body").css("background", "url('assets/imgs/backgrounds/nasa.jpg')");
-    $("body").css("background-size", "cover");
+    styleOptionalBackground();
+});
+
+// Font listeners
+$("#specialElite").click(function(){
+    $("ul").css("font-family", "Special Elite", "cursive");
+    $("#add").css("font-family", "Special Elite", "cursive");
+    $(".title").css("font-family", "Special Elite", "cursive");
+    $(".title").css("width", "80%");
+    $(".progress").css("font-family", "Special Elite", "cursive");
+    $("#clear").css("font-family", "Special Elite", "cursive");
+});
+$("#indieFlower").click(function(){
+    $("ul").css("font-family", "Indie Flower", "cursive");
+    $("#add").css("font-family", "Indie Flower", "cursive");
+    $(".title").css("font-family", "Indie Flower", "cursive");
+    $(".title").css("width", "80%");
+    $(".progress").css("font-family", "Indie Flower", "cursive");
+    $("#clear").css("font-family", "Indie Flower", "cursive");
+});
+$("#luckiestGuy").click(function(){
+    $("ul").css("font-family", "Luckiest Guy", "cursive");
+    $("#add").css("font-family", "Luckiest Guy", "cursive");
+    $(".title").css("font-family", "Luckiest Guy", "cursive");
+    $(".title").css("width", "80%");
+    $(".progress").css("font-family", "Luckiest Guy", "cursive");
+    $("#clear").css("font-family", "Luckiest Guy", "cursive");
+});
+$("#lobster").click(function(){
+    $("ul").css("font-family", "Lobster", "cursive");
+    $("#add").css("font-family", "Lobster", "cursive");
+    $(".title").css("font-family", "Lobster", "cursive");
+    $(".title").css("width", "80%");
+    $(".progress").css("font-family", "Lobster", "cursive");
+    $("#clear").css("font-family", "Lobster", "cursive");
+});
+$("#permanentMarker").click(function(){
+    $("ul").css("font-family", "Permanent Marker", "cursive");
+    $("#add").css("font-family", "Permanent Marker", "cursive");
+    $(".title").css("font-family", "Permanent Marker", "cursive");
+    $(".title").css("width", "80%");
+    $(".progress").css("font-family", "Permanent Marker", "cursive");
+    $("#clear").css("font-family", "Permanent Marker", "cursive");
 });
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
